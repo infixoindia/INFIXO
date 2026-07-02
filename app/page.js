5b6e55e2f494a3c7363945bf73bfa96294af2342
infixoindia <zerotooneyt9@gmail.com>
2026-07-02 15:16:34 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main style={{ textAlign: 'center', padding: '4rem 1rem', fontFamily: 'sans-serif' }}>
+      <h1>Infixo</h1>
+      <p>Apno Se Judne Ka Naya Tarika</p>
+      <p>Coming soon 🚀</p>
+    </main>
+  );
+}
