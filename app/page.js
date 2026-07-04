0bc51649f590df497c53320a4b2efd9459b90a15
infixoindia <zerotooneyt9@gmail.com>
2026-07-04 16:22:27 +0530

    add header to homepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
+import Header from './components/Header/Header';
+
 export default function HomePage() {
   return (
-    <main style={{ textAlign: 'center', padding: '4rem 1rem', fontFamily: 'sans-serif' }}>
-      <h1>Infixo</h1>
-      <p>Apno Se Judne Ka Naya Tarika</p>
-      <p>Coming soon 🚀</p>
+    <main>
+      <Header />
     </main>
   );
 }
