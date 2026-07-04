5e7a14d60661026f5a12724b51951c15f30e31d8
infixoindia <zerotooneyt9@gmail.com>
2026-07-04 16:17:46 +0530

    create header component

---
+++ app/app/components/Header/Header.js
@@ -0,0 +1,13 @@
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <img
+        src="/images/logo.png"
+        alt="Infixo Logo"
+        className={styles.logo}
+      />
+    </header>
+  );
+}
