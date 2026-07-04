2018bc173c58a80d02e92d1e4d89864fcd1d0668
infixoindia <zerotooneyt9@gmail.com>
2026-07-04 16:46:21 +0530

    fix header component export and structure

--- app/app/components/Header/Header.js
+++ app/app/components/Header/Header.js
@@ -3,11 +3,7 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <img
-        src="/images/logo.png"
-        alt="Infixo Logo"
-        className={styles.logo}
-      />
+      <img src="/images/logo.png" alt="Infixo Logo" className={styles.logo} />
     </header>
   );
 }
