b549d0888a9171892a4221731a2e119adeab604a
infixoindia <zerotooneyt9@gmail.com>
2026-07-04 19:36:14 +0530

    Center logo in header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,7 +1,13 @@
 export default function Header() {
   return (
-    <header>
-      <img src="/images/logo.png" alt="Infixo Logo" width="160" />
+    <header
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        padding: "24px 0",
+      }}
+    >
+      <img src="/images/logo.png" alt="Infixo Logo" width="180" />
     </header>
   );
 }
