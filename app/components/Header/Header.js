655a83f2a8ec69667fde6f899df27f5e2a2db9bd
infixoindia <zerotooneyt9@gmail.com>
2026-07-05 15:12:56 +0530

    Add tagline to header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -8,6 +8,10 @@ export default function Header() {
         alt="Infixo Logo"
         className={styles.logo}
       />
+
+      <p className={styles.tagline}>
+        Apno Se Judne Ka Naya Tarika
+      </p>
     </header>
   );
 }
