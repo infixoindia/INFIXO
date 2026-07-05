a4c22b28a62dc33d7038ef8a5cb746ccc4bd04bc
infixoindia <zerotooneyt9@gmail.com>
2026-07-05 17:18:04 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,22 +1,14 @@
-import Header from "./components/Header/Header";
-import WorkerIdentityCard from "./components/Header/WorkerIdentityCard";
+import Header from './components/Header/Header';
+import WorkerIdentityCard from './components/WorkerIdentityCard/WorkerIdentityCard';
+import dummyWorker from './data/dummyWorker';
 
 export default function HomePage() {
   return (
-    <main style={{ background: "#F6F7F9", minHeight: "100vh" }}>
-      
-      {/* Header Section */}
+    <main>
       <Header />
-
-      {/* Profile Section Wrapper */}
-      <div style={{
-        display: "flex",
-        justifyContent: "center",
-        padding: "24px 16px"
-      }}>
-        <WorkerIdentityCard />
+      <div style={{ padding: '2rem 1rem' }}>
+        <WorkerIdentityCard worker={dummyWorker} />
       </div>
-
     </main>
   );
 }
