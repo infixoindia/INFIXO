0087d6d1d490f032b10729f01e6a50f6e187dd7c
infixoindia <zerotooneyt9@gmail.com>
2026-07-05 16:00:05 +0530

    Show WorkerIdentityCard on homepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import WorkerIdentityCard from "./components/WorkerIdentityCard";
 
 export default function HomePage() {
   return (
     <main>
       <Header />
+      <WorkerIdentityCard />
     </main>
   );
 }
