8d133e70dc0950972d69e5718fef3284df9c0887
infixoindia <zerotooneyt9@gmail.com>
2026-07-05 17:12:34 +0530

    Create dummyWorker.js

---
+++ app/data/dummyWorker.js
@@ -0,0 +1,10 @@
+const dummyWorker = {
+  photoUrl: '/images/worker-placeholder.png',
+  fullName: 'Rahul Sharma',
+  isVerified: true,
+  profession: 'Electrician',
+  experience: 8,
+  serviceArea: ['Indore', 'Dewas'],
+};
+
+export default dummyWorker;
