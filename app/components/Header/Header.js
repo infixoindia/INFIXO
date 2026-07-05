8db2a32ff619322de97d57a683b31f440e43a701
infixoindia <zerotooneyt9@gmail.com>
2026-07-05 14:50:37 +0530

    Use CSS module for Header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,13 +1,13 @@
+import styles from "./Header.module.css";
+
 export default function Header() {
   return (
-    <header
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        padding: "24px 0",
-      }}
-    >
-      <img src="/images/logo.png" alt="Infixo Logo" width="180" />
+    <header className={styles.header}>
+      <img
+        src="/images/logo.png"
+        alt="Infixo Logo"
+        className={styles.logo}
+      />
     </header>
   );
 }
