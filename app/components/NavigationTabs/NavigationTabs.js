7191cdf6c40ae0f411d34d2fa517cc08ca94d7e5
infixoindia <zerotooneyt9@gmail.com>
2026-07-12 15:03:25 +0530

    Update NavigationTabs.js

--- app/components/NavigationTabs/NavigationTabs.js
+++ app/components/NavigationTabs/NavigationTabs.js
@@ -2,8 +2,24 @@ import styles from './NavigationTabs.module.css';
 
 export default function NavigationTabs() {
   return (
-    <div>
-      Navigation Tabs
+    <div className={styles.grid}>
+
+      <button className={`${styles.card} ${styles.blue}`}>
+        Work Details
+      </button>
+
+      <button className={`${styles.card} ${styles.orange}`}>
+        Worker Details
+      </button>
+
+      <button className={`${styles.card} ${styles.green}`}>
+        Work Photos
+      </button>
+
+      <button className={`${styles.card} ${styles.purple}`}>
+        Work Videos
+      </button>
+
     </div>
   );
 }
