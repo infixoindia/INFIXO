b137c1cf8304cbb7d48d23961b88373db6ea0da8
infixoindia <zerotooneyt9@gmail.com>
2026-07-14 15:06:34 +0530

    Update page.js

--- app/work-details/page.js
+++ app/work-details/page.js
@@ -1,1 +1,14 @@
+import Header from "../components/Header/Header";
+import WorkDetails from "../components/WorkDetails/WorkDetails";
 
+export default function WorkDetailsPage() {
+  return (
+    <main>
+      <Header />
+
+      <div style={{ padding: "2rem 1rem" }}>
+        <WorkDetails />
+      </div>
+    </main>
+  );
+}
