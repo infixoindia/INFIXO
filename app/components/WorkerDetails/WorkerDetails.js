28ad437055cb91e78f82d808eb948b9525846076
infixoindia <zerotooneyt9@gmail.com>
2026-07-26 14:47:48 +0530

    Update WorkerDetails.js

--- app/components/WorkerDetails/WorkerDetails.js
+++ app/components/WorkerDetails/WorkerDetails.js
@@ -18,36 +18,39 @@ export default function WorkerDetails() {
 
   <div className={styles.detailsBody}>
 
-    <div className={styles.detaiRow}>
-      <div className={styles.label}>Full Name</div>
-      <div className={styles.value}>
-  <span className={styles.detailText}>Rahul Sharma</span>
-</div>
+  <div className={styles.detailRow}>
+    <div className={styles.label}>Full Name</div>
+    <div className={styles.value}>
+      <span className={styles.detailText}>Rahul Sharma</span>
+    </div>
+  </div>
 
-    <div className={styles.detailRow}>
-      <div className={styles.label}>Gender</div>
-      <div className={styles.value}>
-  <span className={styles.detailText}>Male</span>
-</div>
+  <div className={styles.detailRow}>
+    <div className={styles.label}>Gender</div>
+    <div className={styles.value}>
+      <span className={styles.detailText}>Male</span>
+    </div>
+  </div>
 
-    <div className={styles.detailRow}>
-      <div className={styles.label}>Age</div>
-      <div className={styles.value}>
-  <span className={styles.detailText}>28 Years</span>
-</div>
+  <div className={styles.detailRow}>
+    <div className={styles.label}>Age</div>
+    <div className={styles.value}>
+      <span className={styles.detailText}>28 Years</span>
+    </div>
+  </div>
 
-    <div className={styles.detailRow}>
-      <div className={styles.label}>Address</div>
-      <div className={styles.value}>
-  <span className={styles.detailText}>Indore, Madhya Pradesh</span>
-</div>
+  <div className={styles.detailRow}>
+    <div className={styles.label}>Address</div>
+    <div className={styles.value}>
+      <span className={styles.detailText}>Indore, Madhya Pradesh</span>
+    </div>
+  </div>
 
-    <div className={styles.detailRow}>
-      <div className={styles.label}>Languages</div>
-      <div className={styles.value}>
-  <span className={styles.detailText}>Hindi, English</span>
-</div>
-
+  <div className={styles.detailRow}>
+    <div className={styles.label}>Languages</div>
+    <div className={styles.value}>
+      <span className={styles.detailText}>Hindi, English</span>
+    </div>
   </div>
 
 </div>
