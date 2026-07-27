f5668913c28768d107d653c4eeef196bf790eee8
infixoindia <zerotooneyt9@gmail.com>
2026-07-27 14:51:49 +0530

    Update WorkerDetails.js

--- app/components/WorkerDetails/WorkerDetails.js
+++ app/components/WorkerDetails/WorkerDetails.js
@@ -130,6 +130,8 @@ export default function WorkerDetails() {
     {workerOpen ? "⌃" : "⌄"}
   </div>
 </div>
+    
+</div> 
 
       <div className={`${styles.verifyBadge} ${styles.blue}`}>
         <div className={styles.verifyIcon}>
