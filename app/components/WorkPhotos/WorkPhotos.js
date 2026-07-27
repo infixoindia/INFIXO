091a3435f0f67fdee32824679206a3442fe93278
infixoindia <zerotooneyt9@gmail.com>
2026-07-27 23:34:14 +0530

    Create WorkPhotos.js

---
+++ app/components/WorkPhotos/WorkPhotos.js
@@ -0,0 +1,16 @@
+"use client";
+
+import styles from "./WorkPhotos.module.css";
+
+export default function WorkPhotos() {
+  return (
+    <section className={styles.wrapper}>
+
+      <div className={styles.header}>
+        <h2>Work Gallery</h2>
+        <p>Professional work completed by the worker.</p>
+      </div>
+
+    </section>
+  );
+}
