b00426eddc0e5c36bab09e57094c143aff1cb6aa
infixoindia <zerotooneyt9@gmail.com>
2026-07-28 14:09:13 +0530

    Update WorkPhotos.js

--- app/components/WorkPhotos/WorkPhotos.js
+++ app/components/WorkPhotos/WorkPhotos.js
@@ -32,6 +32,34 @@ export default function WorkPhotos() {
 
       </div>
 
+                <div className={styles.gallery}>
+
+  <div className={styles.photoItem}>
+    <img src="/images/work1.jpg" alt="Work 1" />
+  </div>
+
+  <div className={styles.photoItem}>
+    <img src="/images/work2.jpg" alt="Work 2" />
+  </div>
+
+  <div className={styles.photoItem}>
+    <img src="/images/work3.jpg" alt="Work 3" />
+  </div>
+
+  <div className={styles.photoItem}>
+    <img src="/images/work4.jpg" alt="Work 4" />
+  </div>
+
+  <div className={styles.photoItem}>
+    <img src="/images/work5.jpg" alt="Work 5" />
+  </div>
+
+  <div className={styles.photoItem}>
+    <img src="/images/work6.jpg" alt="Work 6" />
+  </div>
+
+</div>
+
     </section>
   );
 }
