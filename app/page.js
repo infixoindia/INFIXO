2c0e70c7bc38ea3ba6e32d99134f70ed5ced34c8
infixoindia <zerotooneyt9@gmail.com>
2026-08-05 16:47:29 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,20 @@
-import WorkDetails from "./components/WorkDetails/WorkDetails";
-import NavigationTabs from './components/NavigationTabs/NavigationTabs';
-import Header from './components/Header/Header';
-import WorkerIdentityCard from './components/WorkerIdentityCard/WorkerIdentityCard';
-import dummyWorker from './data/dummyWorker';
+import Header from "./components/Header/Header";
+import WorkerIdentityCard from "./components/WorkerIdentityCard/WorkerIdentityCard";
+import NavigationTabs from "./components/NavigationTabs/NavigationTabs";
+import Footer from "./components/Footer/Footer";
+import dummyWorker from "./data/dummyWorker";
 
 export default function HomePage() {
   return (
     <main>
       <Header />
-      <div style={{ padding: '2rem 1rem' }}>
-       
-  <WorkerIdentityCard worker={dummyWorker} />
-  <NavigationTabs />
-  {/* <WorkDetails /> */}
+
+      <div style={{ padding: "2rem 1rem 0 1rem" }}>
+        <WorkerIdentityCard worker={dummyWorker} />
+        <NavigationTabs />
       </div>
+
+      <Footer />
     </main>
   );
 }
