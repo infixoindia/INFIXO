6372a07ac2f8c6500a16cab6695f34f645c08777
infixoindia <zerotooneyt9@gmail.com>
2026-08-05 16:41:01 +0530

    Update Footer.js

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,1 +1,19 @@
+import Image from 'next/image';
+import styles from './Footer.module.css';
 
+export default function Footer() {
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.imageWrapper}>
+        <Image
+          src="/images/footer-img.webp"
+          alt="Workers Connections Banner"
+          width={1200}
+          height={600}
+          priority={false}
+          className={styles.footerImage}
+        />
+      </div>
+    </footer>
+  );
+}
