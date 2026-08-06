9c11437fa67ea38ef584c64590655e7244b108d9
infixoindia <zerotooneyt9@gmail.com>
2026-08-06 23:26:27 +0530

    Update WorkDetails.js

--- app/components/WorkDetails/WorkDetails.js
+++ app/components/WorkDetails/WorkDetails.js
@@ -39,9 +39,14 @@ export default function WorkDetails() {
         <svg
   className={styles.skillIcon}
   viewBox="0 0 24 24"
-  fill="currentColor"
+  fill="none"
+  stroke="currentColor"
+  strokeWidth="2"
+  strokeLinecap="round"
+  strokeLinejoin="round"
 >
-  <path d="M7 14c-1.66 0-3 1.34-3 3 0 1.31-1.16 2-2 2 .92 1.22 2.49 2 4 2 2.21 0 4-1.79 4-4 0-1.66-1.34-3-3-3zm13.71-9.37l-1.34-1.34a.996.996 0 0 0-1.41 0L9 12.25 11.75 15l8.96-8.96c.39-.39.39-1.02 0-1.41z"/>
+  <path d="M18.37 2.63a2.12 2.12 0 0 1 3 3L14 13l-4 1 1-4Z" />
+  <path d="M9 14c-2 3-4 3.5-7 4l8 10c2-1 2-2 2-3Z" />
 </svg>
 
   <span>Painter</span>
