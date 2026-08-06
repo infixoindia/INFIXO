ee16cb439d2526dda58f3c2830e72ac7fdb40b85
infixoindia <zerotooneyt9@gmail.com>
2026-08-06 23:08:08 +0530

    Update WorkDetails.js

--- app/components/WorkDetails/WorkDetails.js
+++ app/components/WorkDetails/WorkDetails.js
@@ -46,7 +46,7 @@ export default function WorkDetails() {
     strokeLinejoin="round"
   >
     <path d="M18.37 2.63L14 7l-1.59-1.59a2 2 0 0 0-2.82 0L8 7l9 9 1.59-1.59a2 2 0 0 0 0-2.82L17 10l4.37-4.37a2.12 2.12 0 1 0-3-3z"/>
-    <path d="M9 8c-2 3-4 3.5-7 4l8 10c2-1 2-2 2-3"/>
+    <path d="M9 8c-2 3-4 3.5-7 4l8 10c2-1 2-2 2-3Z"/>
   </svg>
   <span>Painter</span>
 </div>
