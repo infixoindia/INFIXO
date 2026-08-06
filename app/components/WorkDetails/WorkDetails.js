acd001f143860487eef6ad7f1bd5e8c09bb8dafa
infixoindia <zerotooneyt9@gmail.com>
2026-08-06 23:22:44 +0530

    Update WorkDetails.js

--- app/components/WorkDetails/WorkDetails.js
+++ app/components/WorkDetails/WorkDetails.js
@@ -36,7 +36,7 @@ export default function WorkDetails() {
 
   <div className={styles.value}>
    <div className={styles.primaryBadge}>
-      <svg
+        <svg
     className={styles.skillIcon}
     viewBox="0 0 24 24"
     fill="none"
@@ -45,12 +45,12 @@ export default function WorkDetails() {
     strokeLinecap="round"
     strokeLinejoin="round"
   >
-    {/* Brush Bristles */}
-    <path d="M14 2H6a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z" />
-    {/* Metal Ferrule */}
-    <path d="M4 8h10v3H4z" />
-    {/* Brush Handle */}
-    <path d="M9 11v6a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-1" />
+    {/* Roller Head */}
+    <rect x="4" y="3" width="16" height="5" rx="1.5" />
+    {/* Metal Bracket */}
+    <path d="M4 5.5H2v5a2 2 0 0 0 2 2h8v3" />
+    {/* Handle */}
+    <path d="M12 15.5v5" strokeWidth="2.8" />
   </svg>
   <span>Painter</span>
 </div>
