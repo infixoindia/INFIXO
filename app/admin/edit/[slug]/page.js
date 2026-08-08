8e482642ccdcf99772c7e75f053fc5a5479d1d39
infixoindia <zerotooneyt9@gmail.com>
2026-08-08 18:33:50 +0530

    Update page.js

--- app/admin/edit/[slug]/page.js
+++ app/admin/edit/[slug]/page.js
@@ -2,7 +2,8 @@
 
 import { use, useEffect, useState } from 'react';
 import WorkerMasterProfile from '../../../components/WorkerMasterProfile/WorkerMasterProfile';
-import { supabase } from '../../../../lib/supabase';
+import { supabase } from '../../../lib/supabase';
+
 
 export default function EditWorkerPage({ params }) {
   const { slug } = use(params);
