9ad1a61b41bcab6347a7ea3460d1cfb29bd2e904
infixoindia <zerotooneyt9@gmail.com>
2026-08-08 18:18:49 +0530

    Update page.js

--- app/w/[slug]/page.js
+++ app/w/[slug]/page.js
@@ -1,12 +1,10 @@
-import WorkerMasterProfile from '../../components/WorkerMasterProfile/WorkerMasterProfile';
-import { supabase } from '../../../lib/supabase';
-
+import WorkerMasterProfile from '@/app/components/WorkerMasterProfile/WorkerMasterProfile';
+import { supabase } from '@/lib/supabase';
 import { notFound } from 'next/navigation';
 
 export default async function PublicWorkerProfilePage({ params }) {
   const { slug } = await params;
 
-  // Supabase se worker fetch karo
   const { data: worker, error } = await supabase
     .from('workers')
     .select('*')
@@ -14,7 +12,7 @@ export default async function PublicWorkerProfilePage({ params }) {
     .single();
 
   if (error || !worker) {
-    notFound(); // Agar database me worker na mile toh 404
+    notFound();
   }
 
   return <WorkerMasterProfile initialWorkerData={worker} mode="preview" />;
