1cc1e99a4ef2fd43249fef2f1182b9d47c350612
infixoindia <zerotooneyt9@gmail.com>
2026-08-08 20:34:07 +0530

    Update page.js

--- app/w/[slug]/page.js
+++ app/w/[slug]/page.js
@@ -1,5 +1,5 @@
 import WorkerMasterProfile from '../../components/WorkerMasterProfile/WorkerMasterProfile';
-import { supabase } from '../../../lib/supabase';
+import { supabase } from '../../../lib/supabaseClient';
 
 import { notFound } from 'next/navigation';
 
