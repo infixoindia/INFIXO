08f6383563b5c6fbad0ad7abbde428a8f45a3b40
infixoindia <zerotooneyt9@gmail.com>
2026-08-08 18:25:53 +0530

    Update page.js

--- app/admin/create/page.js
+++ app/admin/create/page.js
@@ -1,7 +1,7 @@
 'use client';
 
-import WorkerMasterProfile from '@/app/components/WorkerMasterProfile/WorkerMasterProfile';
-import { supabase } from '@/lib/supabase';
+import WorkerMasterProfile from '../../components/WorkerMasterProfile/WorkerMasterProfile';
+import { supabase } from '../../../lib/supabase';
 import { useRouter } from 'next/navigation';
 
 export default function CreateWorkerPage() {
