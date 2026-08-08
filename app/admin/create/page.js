a2c94869d8249e82ef3d649f1372ce8b1ed565f7
infixoindia <zerotooneyt9@gmail.com>
2026-08-08 19:06:36 +0530

    Update page.js

--- app/admin/create/page.js
+++ app/admin/create/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import WorkerMasterProfile from '../../components/WorkerMasterProfile/WorkerMasterProfile';
-import { supabase } from '@/lib/supabase';
+import { supabase } from '../../../lib/supabase';
 
 import { useRouter } from 'next/navigation';
 
